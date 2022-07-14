93b92b49b9eeefb642e7d68ac5c0447616d95217
Rafaela Moreira <rafaela.etecabdias@gmail.com>
2022-07-14 16:23:46 -0300

    Alterei o else

--- condicoesaninhadas.js/ex001.js
+++ condicoesaninhadas.js/ex001.js
@@ -4,7 +4,7 @@ if ( idade < 16 ){
     console.log('Não vota')
 }else if( idade < 18 || idade >= 65){
     console.log ('Seu voto é opcional')
-}else {
+}else{
     console.log ('Seu voto é obrigatório')
 }
     
