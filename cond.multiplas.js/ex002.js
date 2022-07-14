18020c86274469316629c97db01ee6b585cc1227
Rafaela Moreira <rafaela.etecabdias@gmail.com>
2022-07-14 17:24:47 -0300

    exercício referente aos meses

---
+++ cond.multiplas.js/ex002.js
@@ -0,0 +1,44 @@
+var mes = 13
+
+console.log('Digite um numero de 0 a 12, equivalente ao número de um mês')
+switch (mes){
+    case 1:
+        console.log('Janeiro')
+        break
+    case 2:
+        console.log('Fevereiro')
+        break
+    case 3:
+        console.log('Março')
+        break
+    case 4: 
+        console.log('Abril')
+        break
+    case 5:
+        console.log('Maio')
+        break
+    case 6:
+        console.loh('Junho')
+        break
+    case 7:
+        console.log('Julho')
+        break
+    case 8 : 
+        console.log('Agosto')
+        break
+    case 9: 
+        console.log('Setembro')
+        break
+    case 10:
+        console.log('Outubro')
+        break
+    case 11:
+        console.log('Novembro')
+        break
+    case 12: 
+        console.log('Dezembro')
+        break
+    default:
+        console.log('Mês inválido!')
+
+}
