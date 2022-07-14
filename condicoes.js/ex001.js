9aa379e0e096b6fb7be36568fa45946967fdff3d
Rafaela Moreira <rafaela.etecabdias@gmail.com>
2022-07-14 15:15:33 -0300

    Initial commit

---
+++ condicoes.js/ex001.js
@@ -0,0 +1,7 @@
+var velocidade = 78
+console.log(`a velocidade do seu carro é ${velocidade}Km/h`)
+
+if(velocidade > 60){
+    console.log('Você ultrapassou o limite de velocidade. MULTADO')
+}    
+console.log('Dirija sempre com segurança!')
---
+++ condicoes.js/ex002.js
@@ -0,0 +1,7 @@
+var pais = 'Brasil'
+console.log(`Vivendo em ${pais}`)
+if (pais == 'Alemanha'){
+    console.log('Você é Alemão!')
+}else{
+    console.log('Você é estrangeiro!')
+}
