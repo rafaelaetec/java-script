f37a5ad4256fb846ec723a5573034d6282fc1a87
Rafaela Moreira <rafaela.etecabdias@gmail.com>
2022-07-25 18:35:22 -0300

    Atualizei os exercícios

--- modelo copy/script.js
+++ modelo copy/script.js
@@ -1,1 +1,12 @@
-window.alert('Olá')
+function verificar(){
+   var data = new Date()
+   var ano = data.getFullYear()
+   var fano = window.document.getElementById('txtano')
+   var res = document.getElementById('res')
+   if(fano.value.lenght == 0 || fano.value > ano){
+            window.alert('Verifique os dados e tente novamente!')
+   }else {
+        alert('ok')
+   }
+
+}
