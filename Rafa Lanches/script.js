e0ce31215bd41eb421531fa2875a06457274f4be
Rafaela Moreira <rafaela.etecabdias@gmail.com>
2022-07-27 19:24:35 -0300

    ex novo

---
+++ Rafa Lanches/script.js
@@ -0,0 +1,8 @@
+function verificar(){
+    var piano = window.document.getElementsByName ('tecla')
+    var local = window.document.getElementsByName ('local')
+    var tipo = ''
+    if(piano == tecla[0] && local == local[0]){
+        
+    }
+}
