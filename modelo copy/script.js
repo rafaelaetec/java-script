ce3b79b49ed9c206d20dca7c8e658e10502207e6
Rafaela Moreira <rafaela.etecabdias@gmail.com>
2022-07-27 16:32:57 -0300

    Atualizei o exercício

--- modelo copy/script.js
+++ modelo copy/script.js
@@ -11,27 +11,36 @@ function verificar(){
       var fsex = window.document.getElementsByName ('radsex')
       var idade = ano - Number(fano.value)
       var genero = ''
-      var img = document.createElement ('img')
-      img.setAttribute ('id','foto')
+      //var img = document.createElement ('img')
+      //img.setAttribute ('id','foto')
       if(fsex[0].checked){
          genero = 'Homem'
          if(idade >= 0 && idade <=12){
             img.setAttribute('src', 'menino.jpg')
-         }else if( idade <= 30){
+         }else if( idade <= 20){
+            //jovem/adolescente
+            img.setAttribute('src','jovemhomem2.jpg')
+         }else if (idade < 35 ) {
             //jovem
             img.setAttribute('src','jovemhomem.jpg')
-         }else if (idade < 50 ) {
+         }else if(idade <=50){
+            //adulto
+            img.setAttribute ('src','adultoo.jpg')
+         }else if (idade > 50 ){
             //idoso
-            img.setAttribute('src','idoso.jpg')
+            img.setAttribute ('src','idoso.jpg')
          }
-      }else if (fsex[1].checked){
+         }else if (fsex[1].checked){
          genero = 'Mulher'
-         if(idade >= 0 && idade <=12){
+         if(idade >= 2 && idade <=12){
             img.setAttribute ('src','menina.jpg') 
-         }else if( idade <= 30){
+         }else if( idade <= 34){
             //jovem
             img.setAttribute('src','jovemmulher.jpg')
-         }else if (idade < 50 ) {
+         }else if (idade >= 35){
+            //adulta
+            img.setAttribute('src','adulta.jpg')
+         }else if (idade > 50 ) {
             //idoso
             img.setAttribute('src','idosa.jpg')
          }
