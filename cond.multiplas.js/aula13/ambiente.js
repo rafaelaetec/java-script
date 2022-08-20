52de1ef6321fc9b1b99e449fc9020c7dd61c4ee2
Rafaela Moreira <rafaela.etecabdias@gmail.com>
2022-08-20 14:31:54 -0300

    Atualizei os exercícios

--- cond.multiplas.js/aula13/ambiente.js
+++ cond.multiplas.js/aula13/ambiente.js
@@ -1,9 +1,12 @@
+
+var c = 1
+do{
+    console.log(`Passo ${c}`)
+    c++
+}while(c < 4)
+
 /*var c = 1
 while (c <= 3){
     console.log(`Passo ${c}`)
     c++ 
 }*/
-var c = 1
-do{
-    console.log(`Passo ${c}`)
-}while(c <=4)
---
+++ cond.multiplas.js/aula14/ex001.js
@@ -0,0 +1,19 @@
+
+//estrutura while
+/*var c = 1 
+while ( c <= 4){
+    console.log(`Passo ${c}`)
+    c++
+}*/
+
+//estrutura do while
+/*var c = 1
+do {
+    console.log(`Passo ${c}`)
+    c++
+}while( c <=10)*/
+
+//estrutura FOR
+for (c = 1; c <= 5; c++){
+    console.log(`Passo ${c}`)
+}
